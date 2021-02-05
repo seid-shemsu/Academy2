17b49fdb2e6bcd769f073bf6ec1de5242d3e92ca
Seid <seidshemsu486@yahoo.com>
2021-02-05 08:18:53 +0300

    commit #9 quiz problem solved manifest file ignored

---
+++ app/src/main/java/com/example/academy/database/om.java
@@ -0,0 +1,23 @@
+package com.example.academy.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class om extends SQLiteOpenHelper {
+    public om(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
